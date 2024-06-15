dbab8bd432e988148ae2ca4b0cec3f51df3dccd5
chhartmann <chhartmann@web.de>
2024-06-15 10:45:49 +0200

    Update berry_cycle.sh

--- berry_cycle.sh
+++ berry_cycle.sh
@@ -1,6 +1,9 @@
 TASMOTA_HOST=SmlTestTasmota
+curl -s -o /dev/null "http://$TASMOTA_HOST/ufsd?delete=/smlparser.be"; echo
+curl -s -o /dev/null "http://$TASMOTA_HOST/ufsd?delete=/smlparser.ber"; echo
 curl -s -o /dev/null "http://$TASMOTA_HOST/ufsd?delete=/smartmeter.be"; echo
 curl -s -o /dev/null "http://$TASMOTA_HOST/ufsd?delete=/smartmeter.ber"; echo
+curl -v -s -o /dev/null -F "file=@./smlparser.be" "http://$TASMOTA_HOST/ufsu"; echo
 curl -v -s -o /dev/null -F "file=@./smartmeter.be" "http://$TASMOTA_HOST/ufsu"; echo
 curl http://$TASMOTA_HOST/cm --data-urlencode 'cmnd=brrestart'; echo
 sleep 2
